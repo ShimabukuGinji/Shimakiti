797e37ce47c77585388a7813ef910c6c49b5154a
nomura <yamada@xxx.com>
2024-06-14 12:43:29 +0900

    a

---
+++ src/test/java/com/example/shimakiti/ShimakitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shimakiti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShimakitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
