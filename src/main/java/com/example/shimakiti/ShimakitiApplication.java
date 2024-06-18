3e532728455990b389d030c716c0f343df7d44ec
shinki <GS@email.co.jp>
2024-06-18 17:50:52 +0900

    menu

---
+++ src/main/java/com/example/shimakiti/ShimakitiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shimakiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimakitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShimakitiApplication.class, args);
+
+	}
+}
