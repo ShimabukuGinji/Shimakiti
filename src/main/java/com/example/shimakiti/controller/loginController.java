c2ebe51c419d268beff4d35ce51524ee675d8993
當田大翔 <touda@example.com>
2024-06-21 14:10:09 +0900

    atart

--- src/main/java/com/example/shimakiti/controller/loginController.java
+++ src/main/java/com/example/shimakiti/controller/loginController.java
@@ -1,4 +1,14 @@
 package com.example.shimakiti.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class loginController {
+
+    @GetMapping("sample")
+    public String index() {
+        return "user-add";
+    }
 }
